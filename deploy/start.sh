5a749617fae2e614f89548260c64c340f74bb0cc
Nikolay Gumanov <morgenpurple@gmail.com>
2016-06-02 14:45:25 +0300

    change supervisor config file

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,20 +2,20 @@
 
 NAME=atyichu
 BASEDIR =/home/django
-ENVDIR=${BASEDIR}/venv
-DJANGODIR=${BASEDIR}/${NAME}/businesscenter
-SOCKFILE=/tmp/${NAME}.sock
+ENVDIR="${BASEDIR}/venv"
+DJANGODIR="${BASEDIR}/${NAME}/businesscenter"
+SOCKFILE="/tmp/${NAME}.sock"
 NUM_WORKERS=3
-DJANGO_WSGI_MODULE=${NAME}.wsgi
+DJANGO_WSGI_MODULE="${NAME}.wsgi"
 GUNICORN=gunicorn
 
 cd $DJANGODIR
-source ${ENVDIR}/bin/activate
+source "${ENVDIR}/bin/activate"
 
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
-exec ${GUNICORN} ${DJANGO_WSGI_MODULE}:application \
+exec $GUNICORN "${DJANGO_WSGI_MODULE}:application" \
   --env DJANGO_SETTINGS_MODULE=settings.production_test \
   --workers $NUM_WORKERS \
-  --bind unix:${SOCKFILE} \
+  --bind "unix:${SOCKFILE}" \
 
