d52635d2fec683b0f06827023fd7e808034f706b
Nikolay Gumanov <morgenpurple@gmail.com>
2016-06-03 19:22:17 +0300

    fix route

--- businesscenter/visitor/urls.py
+++ businesscenter/visitor/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = (
-    url(r'^$', views.index_, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login_view, name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^openid$', views.openid_, name='openid'),
