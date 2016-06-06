0eff2b30292b0b2a2f400630a19b357c812e8597
Nikolay Gumanov <morgenpurple@gmail.com>
2016-06-06 11:56:38 +0300

    prepare to test mirror list

--- businesscenter/static/app/en/mirror/controllers.js
+++ businesscenter/static/app/en/mirror/controllers.js
@@ -1,8 +1,9 @@
-angular.module('mirror.controllers', [])
+angular.module('mirror.controllers', ['mirror.services'])
 .controller('CtrlMirror', ['$scope', '$rootScope', '$http',
-'$location', 'Auth',
-    function($scope, $rootScope, $http, $location, Auth) {
+'$location', 'Auth', 'Mirror',
+    function($scope, $rootScope, $http, $location, Auth, Mirror) {
         $rootScope.title = 'Mirror page';
-        $rootScope.alerts.push({ type: 'info', msg: 'Welcome, stranger!' });
+        $rootScope.alerts.push({ type: 'info', msg: 'You  view mirror list!' });
+        $scope.mirrors = Mirror.query();
     }
 ]);
