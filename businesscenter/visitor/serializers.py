162e113ca425f8d5693bbc0aaaa39f361c893197
Nikolay Gumanov <morgenpurple@gmail.com>
2016-06-24 17:24:52 +0300

    attempt to fix group display

--- businesscenter/visitor/serializers.py
+++ businesscenter/visitor/serializers.py
@@ -70,7 +70,7 @@ class WeixinSerializer(serializers.ModelSerializer):
         model = Visitor
         fields = ('weixin', 'avatar_url', 'nickname', 'thumb', 'username',
                   'access_token', 'expires_in', 'refresh_token', 'avatar',
-                  'group_count', 'photo_count')
+                  'group_count', 'photo_count', 'pk')
         extra_kwargs = {'thumb': {'read_only': True},
                         'weixin': {'write_only': True},
                         'access_token': {'write_only': True},
