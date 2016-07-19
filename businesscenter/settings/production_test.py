40416c6488ac0bf398855108a031bd2c7baaf142
Nikolay Gumanov <morgenpurple@gmail.com>
2016-07-19 12:17:59 +0300

    remove my mailgun keys

--- businesscenter/settings/production_test.py
+++ businesscenter/settings/production_test.py
@@ -43,10 +43,9 @@ DATABASES = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-EMAIL_BACKEND = 'django_mailgun_mime.backends.MailgunMIMEBackend'
-MAILGUN_API_KEY = data['MAILGUN_API_KEY']
-MAILGUN_DOMAIN_NAME = data['MAILGUN_DOMAIN_NAME']
+#EMAIL_BACKEND = 'django_mailgun_mime.backends.MailgunMIMEBackend'
+MAILGUN_API_KEY = ""
+MAILGUN_DOMAIN_NAME = ""
 
 DEFAULT_FROM_EMAIL = 'post@atyichu.com'
 SERVER_EMAIL = 'beholder@atyichu.com'
