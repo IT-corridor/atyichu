f72766dbfb1a4ae8476e3057f547761bcd604e2f
niklak2 <morgenpurple@gmail.com>
2016-08-20 15:40:15 +0300

    improved async load factory and map directive

--- businesscenter/static/store/partials/asyncload/services.js
+++ businesscenter/static/store/partials/asyncload/services.js
@@ -1,17 +1,28 @@
 angular.module('asyncload.services', [])
-.factory('LoadScript', ['$window', '$document', '$q', '$timeout',
-function($window, $document, $q, $timeout){
+.factory('LoadScript', ['$window', '$document', '$q', '$timeout', '$rootScope',
+function($window, $document, $q, $timeout, $rootScope){
     /* This factory allow to load scripts asynchronous */
-    return function (src, is_api) {
-        var doc = $document[0];
+    return function (src, callback_name, is_api) {
+
+        if (!$rootScope.deferedScripts){
+          $rootScope.deferedScripts = [];
+        }
+        var i = 0, l = $rootScope.deferedScripts.length;
+
+        for (i; i < l; i++ ){
+          if ($rootScope.deferedScripts[i][0] == src){
+            return $rootScope.deferedScripts[i][1];
+          }
+        }
+
+
         var deferred = $q.defer();
+        $rootScope.deferedScripts.push([src, deferred.promise]);
+
+        var doc = $document[0];
         var scripts = doc.querySelectorAll('script');
 
-        $window.init_async = function(){
-            /* A small trick for api loaders trick.
-            It will be called when the api script (not actual script) will be ready */
-            deferred.resolve();
-        }
+
         angular.forEach(scripts,
             function(script) {
                 if (script.src == src){
@@ -37,6 +48,13 @@ function($window, $document, $q, $timeout){
             };
             doc.body.appendChild(script);
         }
+
+        $window[callback_name] = function(){
+            /* A small trick for api loaders trick.
+            It will be called when the api script (not actual script) will be ready */
+            deferred.resolve();
+        }
+
         return deferred.promise;
     }
 }]);
