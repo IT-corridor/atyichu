b7a2d5168d7422ce4f4d28d7896195df4240383b
Dan <dev1@homeprezzo.com>
2016-08-25 03:09:32 +1000

    prerequisite for analytics

--- businesscenter/snapshot/models.py
+++ businesscenter/snapshot/models.py
@@ -282,6 +282,7 @@ class Like(models.Model):
     data and data migration."""
     photo = models.ForeignKey(Photo, verbose_name=_('Photo'))
     visitor = models.ForeignKey('auth.User', verbose_name=_('Visitor'))
+    like_date = models.DateTimeField(_('Date Started Like'), auto_now_add=True)
 
     def __unicode__(self):
         return '{}:{}'.format(self.photo, self.visitor.id)
