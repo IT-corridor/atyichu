90daf00f9d2330fcb62c89ab66a4cd9843aea708
unknown <C:\Program Files\The Bat>
2016-09-20 15:07:21 +0300

    color likes, full width photos, bug_20160920.pdf

--- businesscenter/settings/local.py
+++ businesscenter/settings/local.py
@@ -31,7 +31,7 @@ LANGUAGE_CODE = 'en-US'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'TEST': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
